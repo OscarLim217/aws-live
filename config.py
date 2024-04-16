97064b5df1c0a81fb8395a14e51e67d5d88111e5
OscarLim217 <167161117+OscarLim217@users.noreply.github.com>
2024-04-16 12:51:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cvis4moe8ijv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aacs3273"
+customdb = "employee"
+custombucket = "oscarlimbohtheng-employee"
+customregion = "us-east-1"
+
