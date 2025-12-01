0b13f051cc5609ee1535f155070951bf92ea9bb5
OscarLim217 <167161117+OscarLim217@users.noreply.github.com>
2025-12-01 12:56:27 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c9qm6wssk1hd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "oscarlim-employee"
+customregion = "us-east-1"
+
